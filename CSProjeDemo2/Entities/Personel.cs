749db67521cd5db0d3840982d57de3ee102fd836
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-03-31 11:12:24 +0300

    Add project files.

---
+++ CSProjeDemo2/Entities/Personel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo2.Entities
+{
+    public abstract class Personel
+    {
+        public int Id { get; set; }
+        public int TCNO { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string? Aciklama { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public DateTime IseBaslamaTarihi { get; set; }
+        public DateTime? IsiBirakmaTarihi { get; set; }
+    }
+}
